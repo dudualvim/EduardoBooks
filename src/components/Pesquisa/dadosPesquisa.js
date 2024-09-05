78703cc5c3fac46e6207c03dce0013fe00eb00e1
Eduardo Alvim <dudualvim20@gmail.com>
2024-09-04 23:02:38 -0300

    Arrumando nomde do Livro

--- src/components/Pesquisa/dadosPesquisa.js
+++ src/components/Pesquisa/dadosPesquisa.js
@@ -10,7 +10,7 @@ export const livros = [
     {nome: 'JavaScript Assertivo', id: 2, src:livro2},
     {nome: 'Teoria dos Grafos', id: 3, src:livro3},
     {nome: 'Arquitetura Limpa', id: 4, src:livro4},
-    {nome: 'Por trás da TI Java', id: 5, src:livro5},
+    {nome: 'Por trás da TI', id: 5, src:livro5},
     {nome: 'Cangaceiro JavaScript', id: 6, src:livro6},
 ]
 
